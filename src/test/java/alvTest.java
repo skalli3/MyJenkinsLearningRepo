ba0afdd94f2345ffa36579de8bd82ceb08dd6ae6
skalli3 <askalli@et.esiea.fr>
2022-02-10 13:33:20 +0100

    Update alvTest.java

--- src/test/java/alvTest.java
+++ src/test/java/alvTest.java
@@ -4,6 +4,15 @@ public class alvTest {
 
     public static void main(String[] args) {
         System.out.println("Jenkins Learning");
+        System.out.println(" _____________________________________________________");
+        System.out.println("|                                                     |");
+        System.out.println("|             FACEBOOK.  V1.0                         |");
+        System.out.println("|                                                     |");
+        System.out.println("|    ---------              ----------                |");
+        System.out.println("|    |Sign In|              |Sign up |                |");
+        System.out.println("|    ---------              ----------                |");
+        System.out.println("|                                                     |");
+        System.out.println("|_____________________________________________________|);
     }
 
 }
