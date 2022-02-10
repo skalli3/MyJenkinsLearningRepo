8f8928bdb605c4f8e2b700c46c1126a7be11aef0
skalli3 <askalli@et.esiea.fr>
2022-02-10 13:56:27 +0100

    Update alv.java

--- src/main/java/alv.java
+++ src/main/java/alv.java
@@ -1,2 +1,15 @@
 public class alv {
+   public static void main(String[] args) {
+        System.out.println("Jenkins Learnffffffffffffing");
+        System.out.println(" _____________________________________________________");
+        System.out.println("|                                                     |");
+        System.out.println("|             FACEBOOK.  V1.0                         |");
+        System.out.println("|                                                     |");
+        System.out.println("|    ---------              ----------                |");
+        System.out.println("|    |Sign In|              |Sign up |                |");
+        System.out.println("|    ---------              ----------                |");
+        System.out.println("|                                                     |");
+        System.out.println("|_____________________________________________________|");
+    }
+
 }
