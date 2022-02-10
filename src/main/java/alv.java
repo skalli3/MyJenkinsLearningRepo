857bc2763823d59de5c95a287d262caf3fb6f8c2
skalli3 <askalli@et.esiea.fr>
2022-02-10 14:33:23 +0100

    Update alv.java

--- src/main/java/alv.java
+++ src/main/java/alv.java
@@ -1,15 +1,45 @@
 public class alv {
    public static void main(String[] args) {
-        System.out.println("Jenkins Learnffffffffffffing");
+     /* System.out.println("                                                      ");
+        System.out.println("                                                      ");
+        System.out.println("                                                      ");
         System.out.println(" _____________________________________________________");
         System.out.println("|                                                     |");
-        System.out.println("|             FACEBOOK.  V1.0                         |");
+        System.out.println("|   FACEBOOK.  V1.0                                   |");
         System.out.println("|                                                     |");
         System.out.println("|    ---------              ----------                |");
         System.out.println("|    |Sign In|              |Sign up |                |");
         System.out.println("|    ---------              ----------                |");
-        System.out.println("|                                                     |");
+        System.out.println("|    User Name:                                       |");
+        System.out.println("|    Password:                                        |");
         System.out.println("|_____________________________________________________|");
+        System.out.println("                                                      ");
+        System.out.println("                                                      ");
+        System.out.println("                                                      ");  */
+      
+      
+      
+      
+      
+      
+        System.out.println("                                                      ");
+        System.out.println("                                                      ");
+        System.out.println("                                                      ");
+        System.out.println(" _____________________________________________________");
+        System.out.println("|                                                     |");
+        System.out.println("|  META FACEBOOK.  V 2.0                              |");
+        System.out.println("|                                                     |");
+        System.out.println("|    ---------              ----------                |");
+        System.out.println("|    |Sign In|              |Sign up |                |");
+        System.out.println("|    ---------              ----------                |");
+        System.out.println("|    User Name:                                       |");
+        System.out.println("|    Password:                                        |");
+        System.out.println("|_____________________________________________________|");
+        System.out.println("                                                      ");
+        System.out.println("                                                      ");
+        System.out.println("                                                      ");
+      
+      
     }
 
 }
